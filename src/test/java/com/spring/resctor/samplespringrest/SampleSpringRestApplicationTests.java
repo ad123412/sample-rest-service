99df5f7af399a7a6e1d3a4ddb9956ddef1491680
ad123412 <ad123412@gmail.com>
2019-02-06 14:40:06 +0530

    first commit

---
+++ src/test/java/com/spring/resctor/samplespringrest/SampleSpringRestApplicationTests.java
@@ -0,0 +1,28 @@
+package com.spring.resctor.samplespringrest;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SampleSpringRestApplicationTests {
+
+//    @Test
+//    public void contextLoads() {
+//    }
+
+    @Test
+    public void test1() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void test2() {
+        Assert.assertTrue(true);
+    }
+
+}
+
